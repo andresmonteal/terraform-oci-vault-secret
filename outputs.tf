a2c22fe46f129bbf0e00b53f468206ddf4a1f436
Andres Montealegre <andres.montealegre@oracle.com>
2024-01-16 11:46:22 -0500

    fix: adds outputs for secret content

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,17 @@ output "secret_id" {
   value       = oci_vault_secret.main[0].id
 }
 
-output "public_ssh_key" {
+output "public_ssh_key_id" {
   description = "secret id"
   value       = oci_vault_secret.main[1].id
+}
+
+output "secret_content" {
+  description = "secret content"
+  value       = oci_vault_secret.main[0].secret_content.content
+}
+
+output "public_ssh_key" {
+  description = "secret content"
+  value       = oci_vault_secret.main[1].secret_content.content
 }
